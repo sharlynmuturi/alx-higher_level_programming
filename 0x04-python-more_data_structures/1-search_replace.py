57d3226ed9e11b0b145b3f53df06a087e17320ba
sharlynmuturi <sharlynnmuturi@gmail.com>
2023-03-16 10:02:09 +0000

    Commit for checking

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new = []
+    for n in my_list:
+        if n == search:
+            new.append(replace)
+        else:
+            new.append(n)
+    return(new)
